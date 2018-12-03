04fe799558f4570d83f0f0205773c683d9d8c759
exunckly <3117100+exunckly@users.noreply.github.com>
2018-12-03 21:50:39 +0000

    Recommented

--- day3_2018.R
+++ day3_2018.R
@@ -10,7 +10,7 @@ library(tidyverse)
 day3_data <- readLines("input/day3_input.txt")
 # Make the divider constant - remove spaces and use , throughout
 day3_data <- gsub(" |#","",day3_data)
-day3_data <- gsub("#|@|:|x", ",", day3_data)
+day3_data <- gsub("@|:|x", ",", day3_data)
 day3_colnames <- c("id", "x", "y", "width", "height")
 
 d3t <- as.tibble(day3_data) %>%
@@ -22,7 +22,7 @@ d3t <- as.tibble(day3_data) %>%
   mutate(y1 = y + 1) %>%
   mutate(y2 = y + height)
 
-# "The whole piece of fabric they're working on is a very large square - at least 1000 inches on each side."
+# The '+1' is needed as the x value is the gap from the edge not the co-ordinate of the first square itself.
 
 # Work out the maximum dimensions of the fabric that we need to deal with
 my_fabric <- matrix(0, nrow = max(d3t$y2) + 1, ncol = max(d3t$x2) + 1)
@@ -33,17 +33,15 @@ for (i in seq_along(d3t$id)){
   my_fabric[d3t$x1[i]:d3t$x2[i], d3t$y1[i]:d3t$y2[i]] <- my_fabric[d3t$x1[i]:d3t$x2[i], d3t$y1[i]:d3t$y2[i]] + 1
 }
 
-# Generate frequency table
-my_fabric_freq <- as.tibble(table(my_fabric))
-
-# How many squares are occupied 2 or more times?
-part1 <- my_fabric_freq %>%
+# Generate frequency table and find out how many squares are occupied 2 or more times
+my_fabric_freq <- as.tibble(table(my_fabric)) %>%
   filter(my_fabric >= 2)
-part1_ans <- sum(part1$n)
+
+part1_ans <- sum(my_fabric_freq$n)
 
 # Part 2
 # Now we need to find the ID of the only claim that doesn't overlap with the others
-# Pull out the sum of that section of the matrix and compare it with the area of the claim
+# For each claim, pull out the sum of that section of the matrix and compare it with the area of the claim
 
 for (i in seq_along(d3t$id)){
   d3t$claim_sum[i] <- sum(my_fabric[d3t$x1[i]:d3t$x2[i], d3t$y1[i]:d3t$y2[i]])
@@ -52,7 +50,6 @@ for (i in seq_along(d3t$id)){
 
 part2 <- d3t %>%
   filter(area == claim_sum)
-
 part2_ans <- part2$id
 
 
