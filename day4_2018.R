34fdac83f33fdd2bf3da2c626ad39307948996d1
exunckly <3117100+exunckly@users.noreply.github.com>
2018-12-04 21:24:49 +0000

    Comments

--- day4_2018.R
+++ day4_2018.R
@@ -4,11 +4,10 @@
 library(tidyverse)
 library(lubridate)
 
-# Need to parse the input file
-# sort by time so that the correct wake/sleep goes with the correct guard
-# mutate so that each row has a guard number, sleep start and sleep end
-
-# Bear in mind that we might run into problems if R knows about leap years and daylight savings time?!
+# Need to:
+#   parse the input file
+#   sort by time so that the correct wake/sleep goes with the correct guard
+#   mutate so that each row has a guard number, sleep start and sleep end
 
 # Use Jenny Bryan's untangle2 function
 # http://rpubs.com/jennybc/untangle-tidyeval
@@ -37,12 +36,12 @@ d4t <- as.tibble(d4) %>%
   untangle2("Guard", jumble, my_guard) %>%                               # Extract raw guard data 'headers' to separate column
   mutate(guard_no = parse_number(my_guard)) %>%                          # Extract guard number from guard data
   mutate(my_minute = as.numeric(format(my_datetime, "%M"))) %>%          # Extract minute
-  mutate(jumble = gsub(" ","_",jumble)) %>%                              # Prepare tidy coliumn names
+  mutate(jumble = gsub(" ","_",jumble)) %>%                              # Prepare nice column names
   spread (jumble, my_minute) %>%                                         # Spread, fill and filter to line up times
   fill (falls_asleep, .direction = "down") %>%
   filter (!(is.na(wakes_up) )) %>%
   mutate (asleep_for = wakes_up - falls_asleep) %>%                      # Calculate time asleep
-  select(guard_no:asleep_for)
+  select(guard_no:asleep_for)                                            # Just the columns we need
 
 
 # Part 1
@@ -57,8 +56,8 @@ sleepiest_g <- d4t %>%
 
 # Careful with indexing and the fact that 'awake' means awake
 
-# Funtion that returns a 60-length vector with the kength of time  aguard was asleep during each minute
-# Note that munite 0 is held in location 1 and so on, so subtract 1 from the index at the very end to get the actual minute
+# Funtion that returns a 60-length vector with the length of time a guard was asleep during each minute across the various days
+# Note that minute 0 is held in location 1 and so on, so subtract 1 from the indices this function returns to get the actual minute
 sleeps_across_hour <- function(x, my_guard){
   my_hour <- rep(0, 60) # index 1 is minute 0 etc.
   x <- x %>%
@@ -74,7 +73,7 @@ sleeps_across_hour <- function(x, my_guard){
 my_hour <- sleeps_across_hour(d4t, sleepiest_g$guard_no[1])
 part1_best_times <- which(my_hour %in% max(my_hour)) - 1 # minus 1 to adjust for R indicing from 1
 
-part1_ans <- sleepiest_g$guard_no[1]* part1_best_times
+part1_ans <- sleepiest_g$guard_no[1]* part1_best_times # guard ID * minute
 
 # Part 2 - Of all guards, which guard is most frequently asleep on the same minute?
 # Didn't bother calculating earlier as didn't know we'd need it :-)
@@ -86,6 +85,6 @@ for (i in seq_along(guards)){
   gp2_sleeps[i,] <- sleeps_across_hour (d4t, guards[i])
 }
 
-my_loc <- which(gp2_sleeps == max(gp2_sleeps), arr.ind = TRUE)
+my_loc <- which(gp2_sleeps == max(gp2_sleeps), arr.ind = TRUE) # Need to use == rather than %in% here
 
-part2_ans <- guards[my_loc[1]] * (my_loc[2] - 1)
+part2_ans <- guards[my_loc[1]] * (my_loc[2] - 1) # guard ID * minute
