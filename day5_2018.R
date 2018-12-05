bb17cf6068e05d3794c8ea906c6ed037df69a40f
exunckly <3117100+exunckly@users.noreply.github.com>
2018-12-05 19:54:05 +0000

    Day 5 part 2

--- day5_2018.R
+++ day5_2018.R
@@ -1,15 +1,18 @@
 # Advent of code 2018 day 5
 # https://adventofcode.com/2018/day/5
 
-# Create search pattern
+# Create pairs of patterns for part 2
+my_pairs <- paste(LETTERS,letters, sep = "|")
+
+# Create main search pattern
 pattern <- paste(c(paste0(LETTERS,letters), paste0(letters,LETTERS)), collapse="|")
 
 # Load input
 d5 <- readLines("input/day5_input.txt")
 
+# Function to remove text matching a pattern from a string
 # As new letters will 'bump' together as old ones are removed,
 # use gsub until the string doesn't get any shorter
-
 remove_units <- function(x, pattern){
   exit_loop <- 0
   while (exit_loop == 0){
@@ -22,4 +25,15 @@ remove_units <- function(x, pattern){
   return (x)
 }
 
+# Part 1
 part1 <- nchar(remove_units(d5, pattern))
+
+# Part 2
+atoz_results <- vector(mode = "numeric", length = length(my_pairs))
+for(i in seq_along(my_pairs)){
+  test <- remove_units(gsub(my_pairs[i], "", d5), pattern)
+  print(i)
+}
+
+part2 <- min(atoz_results)
+
